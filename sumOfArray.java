271a224b4f0e78febb41ae096ac8ff399b741763
Shivam Yadav <pralabhyadav9589@gmail.com>
2026-01-01 21:25:03 +0530

    DAY -1 Sum Of Array

---
+++ sumOfArray.java
@@ -0,0 +1,17 @@
+
+// Program to sum all the element of array
+// Date ==> 01/01/2025
+
+public class sumOfArray {
+    public static void main(String[] args){
+      int arry[]={20,15,30,80,4};
+      
+      int sum=0;
+     
+for(int value:arry){
+         sum+=value;
+        }
+System.out.println("sum of element of array is "+sum);
+
+    }
+}
